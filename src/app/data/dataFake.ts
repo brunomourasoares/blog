6f67dd800619f0fe685ef5f83ee552f8bba24dde
Bruno Moura <brunomourasoares@gmail.com>
2023-10-22 19:05:36 -0300

    Desafio Concluido - blog

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+      "id":"1",
+      "title": "DUET NIGHT ABYSS",
+      "description": "O RPG de aventura e fantasia, Duet Night Abyss, revela seu primeiro trailer",
+      "photoCover":"https://uploads.adrenaline.com.br/2023/10/O-RPG-de-aventura-e-fantasia-Duet-Night-Abyss-revela-seu-primeiro-trailer-327x204.jpg"
+    },
+    {
+      "id":"2",
+      "title": "LIKE A DRAGON",
+      "description": "Like a Dragon: Infinite Wealth tem longo gameplay revelado",
+      "photoCover":"https://uploads.adrenaline.com.br/2023/06/like-a-dragon-infinite-wealth-2-327x204.jpg"
+    },
+    {
+      "id":"3",
+      "title": "MY LOVELY EMPRESS",
+      "description": "My Lovely Empress ganha novo trailer",
+      "photoCover":"https://uploads.adrenaline.com.br/2023/10/My-Lovely-Empress-ganha-novo-trailer-327x204.jpg"
+    },
+    {
+      "id":"4",
+      "title": "EFOOTBALL",
+      "description": "eFootball 2024 chega sem times brasileiros; veja principais novidades do jogo",
+      "photoCover":"https://uploads.adrenaline.com.br/2023/10/eFootball-2024-chega-sem-times-brasileiros-veja-principais-novidades-do-jogo-327x204.jpg"
+    }
+  ]  
